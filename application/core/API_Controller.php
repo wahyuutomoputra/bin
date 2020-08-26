d65bc11e84951c727542f7c5f0b345c213819614
wahyuutomoputra@gmail.com <wahyuutomoputra@gmail.com>
2020-08-26 15:06:53 +0700

    cors

--- application/core/API_Controller.php
+++ application/core/API_Controller.php
@@ -12,7 +12,19 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 class API_Controller extends REST_Controller {
     public function __construct()
 	  {
-		  parent::__construct();
+      parent::__construct();
+      if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+          header('Access-Control-Allow-Origin: *');
+          header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+          header('Access-Control-Allow-Headers: token, Content-Type');
+          header('Access-Control-Max-Age: 1728000');
+          header('Content-Length: 0');
+          header('Content-Type: text/plain');
+          die();
+      }
+
+      header('Access-Control-Allow-Origin: *');
+      header('Content-Type: application/json');
     }
     
     public function auth()

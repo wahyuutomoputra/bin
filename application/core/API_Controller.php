7e4c3bff643e9d5d24efdfcaedfba7528b15fda1
wahyuutomoputra@gmail.com <wahyuutomoputra@gmail.com>
2020-08-31 06:55:02 +0700

    cookies

--- application/core/API_Controller.php
+++ application/core/API_Controller.php
@@ -10,9 +10,8 @@ use Restserver\Libraries\REST_Controller;
 //header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 
 class API_Controller extends REST_Controller {
-    public function __construct()
+    public function __construct($config = 'rest')
 	  {
-      $config['allow_any_cors_domain'] = TRUE;
       parent::__construct($config);
       if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
           header('Access-Control-Allow-Origin: *');

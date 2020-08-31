605eb51b2929b835e64599f295f56e06ce9a132c
wahyuutomoputra@gmail.com <wahyuutomoputra@gmail.com>
2020-08-31 07:14:54 +0700

    cookies

--- application/core/API_Controller.php
+++ application/core/API_Controller.php
@@ -14,7 +14,7 @@ class API_Controller extends REST_Controller {
 	  {
       parent::__construct($config);
       if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-          header('Set-Cookie: cross-site-cookie=name; SameSite=None; Secure');
+        
           header('Access-Control-Allow-Origin: *');
           header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
           header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, token");
@@ -26,9 +26,10 @@ class API_Controller extends REST_Controller {
           die();
       }
 
-      header('Set-Cookie: cross-site-cookie=name; SameSite=None; Secure');
+      
       header('Access-Control-Allow-Origin: *');
       header('Access-Control-Allow-Credentials: true');
+      header('Access-Control-Max-Age: 1000');
       header("Access-Control-Allow-Methods: PUT, GET, POST");
       header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
       header('Set-Cookie: cross-site-cookie=name; SameSite=None; Secure');
